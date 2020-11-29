9d5f7ec244ff444a5e0130d84e653be2bc917125
Muhammad Ahsan Izhar <ahsaghalib@gmail.com>
2020-11-29 16:16:11 +0500

    eager & lazy

---
+++ eager.mjs
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+import express from "express";
+
+const url = "https://pomber.github.io/covid19/timeseries.json";
+
+const app = express();
+
+let timeline = null;
+
+async function loadTimeline() {
+  const fromDate = new Date();
+  if (timeline === null) {
+    const r = await fetch(url);
+    timeline = await r.json();
+  }
+  const toDate = new Date();
+  console.log("Loading time: " + (toDate.getTime() - fromDate.getTime()));
+
+  return timeline;
+}
+
+app.get("/:country", async (req, res) => {
+  const result = await loadTimeline();
+  res.send(result[req.params.country]);
+});
+
+loadTimeline();
+
+app.listen(8080, () => console.log("listening on port: 8080"));
